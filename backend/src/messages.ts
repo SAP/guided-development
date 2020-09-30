aa0e673547a69e4645840b8919fbb94b70c58b0d
${configUser} <neta.molcho@sap.com>
2020-09-30 11:36:56 +0300

    adding description to the tool

--- backend/src/messages.ts
+++ backend/src/messages.ts
@@ -1,6 +1,6 @@
 export default {
     channelName: "GuidedDev",
     title: "Guided Development Kit",
-    description: "Guided Development Kit Description",
+    description: "Learn how to execute common development tasks for specific SAP scenarios. The tool provides guides, and suggests snippets and commands or tools you should run to complete your task.",
     noResponse: "No response received."
 }; 
