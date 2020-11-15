2cc898b5087236d9d3105673cce0b01511c73f76
Stanislav Lvovsky <stanislav.lvovsky@sap.com>
2020-11-15 14:17:45 +0200

    Basctl server (#59)
    * add basctl server
    
    * add server
    
    * fix: add server
    
    * refactor: improve basctl server logic

--- bas-platform/src/extension.ts
+++ bas-platform/src/extension.ts
@@ -1,13 +1,14 @@
 import * as vscode from 'vscode';
 import { bas } from './api';
 import { _performAction } from "./actions/performer";
+import { startBasctlServer, closeBasctlServer } from './actions/basctlAction';
 
-const subscriptions: Array<vscode.Disposable>= [];
+const subscriptions: Array<vscode.Disposable> = [];
 
 function performScheduledActions() {
     const actionsSettings = vscode.workspace.getConfiguration();
-    let actionsList: any[] | undefined = actionsSettings.get("actions");
-    if (actionsList && actionsList.length > 0) {
+    const actionsList: any[] | undefined = actionsSettings.get("actions");
+    if (actionsList && actionsList.length) {
         for (const action of actionsList) {
             console.log(`performing action ${action.name} of type ${action.constructor.name}`)
             _performAction(action);
@@ -16,13 +17,17 @@ function performScheduledActions() {
     }
 }
 
-export async function activate(context: vscode.ExtensionContext) {
+export async function activate() {
+    startBasctlServer();
+
     performScheduledActions();
 
     return bas;
 }
 
 export function deactivate() {
+    closeBasctlServer();
+
     for (const subscription of subscriptions) {
         subscription.dispose();
     }
