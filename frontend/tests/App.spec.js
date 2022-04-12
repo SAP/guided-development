b4e7799f5e8500d41aa8c8d260aa4a4ab01c277f
AliceCC01 <101808017+AliceCC01@users.noreply.github.com>
2022-04-12 22:40:20 +0800

    Update App.spec.js

--- frontend/tests/App.spec.js
+++ frontend/tests/App.spec.js
@@ -60,7 +60,42 @@ describe('App.vue', () => {
     expect(wrapper.vm.collections).toEqual("collection");
   })
 
-  it('changeItemsState - method', () => {
+  it("change not exist item's state", () => {
+    wrapper = initComponent(App);
+    const fqid1 = "extName1.extPublisher1.id1";
+    const item1 = {
+        id: "id1",
+        fqid: fqid1,
+        description: "description1",
+        title: "title1",
+        readState: "UNREAD",
+        labels: []
+    };
+    const fqid2 = "extName1.extPublisher1.id2";
+    const item2 = {
+        id: "id2",
+        fqid: fqid2,
+        description: "description2",
+        title: "title2",
+        labels: [],
+        items: [item1]
+    };
+    const collection1 = {
+        id: "id1",
+        title: "colTitle1",
+        description: "description1",
+        itemIds: [],
+        type: 0,
+        items: [item2]
+    };
+    const notExistsItemFqid = "extName1.extPublisher1.null";
+    wrapper = initComponent(App);
+    wrapper.vm.showCollections([collection1]);
+    const ret = wrapper.vm.changeItemsState([{fqid:notExistsItemFqid, readState: "READ"}]);
+    expect(ret).toBeFalsy();
+  })
+
+  it("change exist item's state", () => {
     wrapper = initComponent(App);
     const fqid1 = "extName1.extPublisher1.id1";
     const item1 = {
@@ -90,12 +125,60 @@ describe('App.vue', () => {
     };
     wrapper = initComponent(App);
     wrapper.vm.showCollections([collection1]);
-    wrapper.vm.changeItemsState([{fqid:fqid1, readState: "READ"}]);
     const tarItem = wrapper.vm.getItemByFqid(fqid1);
+    expect(tarItem.readState).toEqual("UNREAD");
+    wrapper.vm.changeItemsState([{fqid:fqid1, readState: "READ"}]);
     expect(tarItem.readState).toEqual("READ");
+  }) 
+
+  it('find an item not exists', () => {
+    const fqid1 = "extName1.extPublisher1.id1";
+    const item1 = {
+        id: "id1",
+        fqid: fqid1,
+        description: "description1",
+        title: "title1",
+        labels: []
+    };
+    const collection1 = {
+        id: "id1",
+        title: "title1",
+        description: "description1",
+        itemIds: [],
+        type: 0,
+        items: [item1]
+    };
+    wrapper = initComponent(App);
+    wrapper.vm.showCollections([collection1]);
+    const notExistsItemFqid = "extName1.extPublisher1.null";
+    const tarItem = wrapper.vm.getItemByFqid(notExistsItemFqid);
+    expect(tarItem).toBeFalsy();
   })
 
-  it('getItemByFqid - method', () => {
+  it('find an item at root level', () => {
+    const fqid1 = "extName1.extPublisher1.id1";
+    const item1 = {
+        id: "id1",
+        fqid: fqid1,
+        description: "description1",
+        title: "title1",
+        labels: []
+    };
+    const collection1 = {
+        id: "id1",
+        title: "colTitle1",
+        description: "description1",
+        itemIds: [],
+        type: 0,
+        items: [item1]
+    };
+    wrapper = initComponent(App);
+    wrapper.vm.showCollections([collection1]);
+    const tarItem = wrapper.vm.getItemByFqid(fqid1);
+    expect(tarItem.title).toEqual("title1")
+  })
+
+  it('find an item at second level', () => {
     const fqid1 = "extName1.extPublisher1.id1";
     const item1 = {
         id: "id1",
@@ -115,7 +198,7 @@ describe('App.vue', () => {
     };
     const collection1 = {
         id: "id1",
-        title: "title1",
+        title: "colTitle1",
         description: "description1",
         itemIds: [],
         type: 0,
@@ -126,6 +209,98 @@ describe('App.vue', () => {
     const tarItem = wrapper.vm.getItemByFqid(fqid1);
     expect(tarItem.title).toEqual("title1")
   })
+  
+  it('find an item at third level', () => {
+    const fqid1 = "extName1.extPublisher1.id1";
+    const item1 = {
+        id: "id1",
+        fqid: fqid1,
+        description: "description1",
+        title: "title1",
+        labels: []
+    };
+    const fqid2 = "extName1.extPublisher1.id2";
+    const item2 = {
+        id: "id2",
+        fqid: fqid2,
+        description: "description2",
+        title: "title2",
+        labels: [],
+        items: [item1]
+    };
+    const fqid3 = "extName1.extPublisher1.id3";
+    const item3 = {
+        id: "id3",
+        fqid: fqid3,
+        description: "description3",
+        title: "title3",
+        labels: [],
+        items: [item2]
+    };
+    const collection1 = {
+        id: "id1",
+        title: "colTitle1",
+        description: "description1",
+        itemIds: [],
+        type: 0,
+        items: [item3]
+    };
+    wrapper = initComponent(App);
+    wrapper.vm.showCollections([collection1]);
+    const tarItem = wrapper.vm.getItemByFqid(fqid1);
+    expect(tarItem.title).toEqual("title1")
+  })
+
+  it('find an item of multiple collections', () => {
+    const fqid1 = "extName1.extPublisher1.id1";
+    const item1 = {
+        id: "id1",
+        fqid: fqid1,
+        description: "description1",
+        title: "title1",
+        labels: []
+    };
+    const fqid2 = "extName1.extPublisher1.id2";
+    const item2 = {
+        id: "id2",
+        fqid: fqid2,
+        description: "description2",
+        title: "title2",
+        labels: [],
+        items: []
+    };
+    
+    const fqid3 = "extName1.extPublisher1.id3";
+    const item3 = {
+        id: "id3",
+        fqid: fqid3,
+        description: "description3",
+        title: "title3",
+        labels: [],
+        items: [item2]
+    };
+    const collection1 = {
+        id: "id1",
+        title: "title1",
+        description: "description1",
+        itemIds: [],
+        type: 0,
+        items: [item1]
+    };
+    const collection2 = {
+        id: "colId2",
+        title: "colTitle2",
+        description: "colDescription2",
+        itemIds: [],
+        type: 0,
+        items: [item3]
+    };
+    wrapper = initComponent(App);
+    wrapper.vm.showCollections([collection1, collection2]);
+    const tarItem = wrapper.vm.getItemByFqid(fqid2);
+    expect(tarItem.title).toEqual("title2")
+  })
+  
   
   it('onAction - method', () => {
     wrapper = initComponent(App, {}, true)
